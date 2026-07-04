5ae069e66884ebdfa93a371dbd2016431b1b4f71
vinsupkishore <kk7694231@gmail.com>
2026-07-04 07:57:22 +0530

    Final Fix

--- src/components/Productcard.jsx
+++ src/components/Productcard.jsx
@@ -1,11 +1,35 @@
-function ProductCard({ product }) {
+function ProductCard({ product, cart, setCart }) {
 
   const addToCart = () => {
-    let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-    cart.push(product);
+    const existing = cart.find(
+      (item) => item._id === product._id
+    );
 
-    localStorage.setItem("cart", JSON.stringify(cart));
+    if (existing) {
+
+      setCart(
+        cart.map((item) =>
+          item._id === product._id
+            ? {
+                ...item,
+                quantity: item.quantity + 1,
+              }
+            : item
+        )
+      );
+
+    } else {
+
+      setCart([
+        ...cart,
+        {
+          ...product,
+          quantity: 1,
+        },
+      ]);
+
+    }
 
     alert("Product Added To Cart");
   };
@@ -13,6 +37,7 @@ function ProductCard({ product }) {
   return (
     <div>
       <h3>{product.name}</h3>
+
       <p>₹{product.price}</p>
 
       <button onClick={addToCart}>
