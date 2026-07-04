aaf558e70fce6dc81e9737c6be68006e790234a9
vinsupkishore <kk7694231@gmail.com>
2026-07-04 13:48:42 +0530

    Updated Admin Login UI

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -1,73 +1,35 @@
-import { useState } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-
-function AdminLogin() {
-  const navigate = useNavigate();
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleAdminLogin = async (e) => {
-    e.preventDefault();
-
-    try {
-      const { data } = await axios.post(
-        "https://nexastore-backend-rzao.vercel.app/api/auth/login",
-        {
-          email,
-          password,
-        }
-      );
-
-      if (data.role !== "admin") {
-        alert("You are not an admin");
-        return;
-      }
-
-      localStorage.setItem("token", data.token);
-      localStorage.setItem("role", data.role);
-      localStorage.setItem("userId", data.userId);
-
-      alert("Admin Login Successful");
-      navigate("/admin");
-
-    } catch (error) {
-      alert(error.response?.data?.message || "Login Failed");
-    }
-  };
-
-  return (
-    <div>
+return (
+  <div className="admin-login-container">
+    <div className="admin-login-box">
       <h1>Admin Login</h1>
 
       <form onSubmit={handleAdminLogin}>
-        <input
-          type="email"
-          placeholder="Admin Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
 
-        <br />
-        <br />
+        <div className="admin-input-box">
+          <input
+            type="email"
+            placeholder="Enter Admin Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
 
-        <input
-          type="password"
-          placeholder="Admin Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+        <div className="admin-input-box">
+          <input
+            type="password"
+            placeholder="Enter Admin Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
 
-        <br />
-        <br />
-
-        <button type="submit">
+        <button type="submit" className="admin-login-btn">
           Login
         </button>
+
       </form>
     </div>
-  );
-}
-
-export default AdminLogin;
+  </div>
+);
