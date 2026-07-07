cc270c335dc5af20ebf9e09d661e53e2fd1f9dfb
vinsupkishore <kk7694231@gmail.com>
2026-07-07 14:37:39 +0530

    Fixed product section animation and scroll behavior

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -350,6 +350,7 @@ function Home({ cart, setCart }) {
       {/* Featured Products */}
 
       <motion.div
+      ref={productsRef}
   initial={{ opacity: 0, y: 80 }}
   animate={{ opacity: 1, y: 0 }}
   transition={{
